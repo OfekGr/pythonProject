809069611ce128da67e8140e19f16d66c8c7eb35
Ofek <ofek25399@gmail.com>
2021-10-27 17:49:35 +0300

    yes

--- game_cards/Card.py
+++ game_cards/Card.py
@@ -1,7 +1,10 @@
+
 class Card:
+
     def __init__(self, value, suit):
         self.value=value
         self.suit=suit
+
 
     def __gt__(self, current_card, other_card):
         if current_card > other_card:
